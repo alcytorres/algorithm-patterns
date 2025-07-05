0ca014dfe538260c9a91b195d31ade80f58d598f
Alcy Torres <mxalcytorres@gmail.com>
2025-07-05 14:27:36 -0400

    Update solutions

--- ztest.py
+++ ztest.py
@@ -2,17 +2,15 @@ class Human:
     def __init__(self, age, name):
         self.age = age
         self.name = name
-
+    
     def __str__(self):
         return f"A human with name {self.name}. Their age is {self.age}."
 
 
-h = Human(23, "John")
+h = Human(21, "John")
 print(h)
 
 
-
-
-num = 123
-string_rep = str(num)
-print(type(string_rep))  # Output: '123'
+# num = 123
+# string_rep = str(num)
+# print(type(string_rep))  # Output: '123'
