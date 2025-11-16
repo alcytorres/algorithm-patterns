a87d863af748bf7b19a12090fd6197c93e99b03e
Alcy Torres <mxalcytorres@gmail.com>
2025-11-16 00:04:33 -0500

    Update solutions

--- wtest.py
+++ wtest.py
@@ -2,3 +2,18 @@
 
 
 
+
+
+
+
+
+# from collections import deque
+
+# queue = deque()         # []
+# queue.append(1)         # Enqueue → [1]
+# queue.append(2)         # Enqueue → [1, 2]
+# queue.append(3)         # Enqueue → [1, 2, 3]
+
+# print(queue.popleft())  # Dequeue → removes 1
+# print(queue)            # [2, 3]
+
