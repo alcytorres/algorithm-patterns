65912b141a751a7565f4f1e8fdb94e63b81c0d20
Alcy Torres <mxalcytorres@gmail.com>
2026-02-07 20:52:10 -0500

    Update solutions

--- DSA/02_Arrays and Strings/02-9_example_2.py
+++ DSA/02_Arrays and Strings/02-9_example_2.py
@@ -1,8 +1,9 @@
 # Example 2: Longest Substring with At Most One "0"
+"""
+Finds the longest substring with at most one "0" by flipping at most one "0" to "1". 
 
-# Finds the longest substring with at most one "0" by flipping at most one "0" to "1". 
-
-# In other words "what is the longest substring that contains at most one "0"?
+In other words "what is the longest substring that contains at most one "0"?
+"""
 
 def longest_substring_one_zero(s):
     left = curr = ans = 0        
