00f2c4fcf85e3781a6bc027e68a5753c88bcd183
Alcy Torres <mxalcytorres@gmail.com>
2026-02-10 23:52:39 -0500

    Update solutions

--- DSA/02_Arrays and Strings/02-15_example_1.py
+++ DSA/02_Arrays and Strings/02-15_example_1.py
@@ -1,12 +1,13 @@
 # Query Subarray Sums with Limit
+"""
+Example 1: Given an integer array nums, an array queries where queries[i] = [x, y] and an integer limit, return a boolean array that represents the answer to each query. A query is true if the sum of the subarray from x to y is less than limit, or false otherwise.
 
-# Example 1: Given an integer array nums, an array queries where queries[i] = [x, y] and an integer limit, return a boolean array that represents the answer to each query. A query is true if the sum of the subarray from x to y is less than limit, or false otherwise.
-
-# Example:
-    # nums = [1, 6, 3, 2, 7, 2]
-    # queries = [[0, 3], [2, 5], [2, 4]]
-    # limit = 13
-    # Output: [True, False, True]
+Example:
+    nums = [1, 6, 3, 2, 7, 2]
+    queries = [[0, 3], [2, 5], [2, 4]]
+    limit = 13
+    Output: [True, False, True]
+"""
 
 def answer_queries(nums, queries, limit):
     prefix = [nums[0]]
