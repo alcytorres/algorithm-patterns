eeddd755fc605029ec84d55eb50b859398f34251
Alcy Torres <mxalcytorres@gmail.com>
2026-03-06 17:43:13 -0500

    Update solutions

--- wtest.py
+++ wtest.py
@@ -4,3 +4,10 @@
 text = 'sky'
 text.encode("utf-8")  -> bytes
 bytes.decode("utf-8") -> str
+
+
+"""
+Check how important the algos here algorithms.py are w CLAUDE
+
+check on CLAUDE if the examples at the bottom of 09-0_Binary Search.py are worth knowing or if the 2 I practice are enough
+"""
