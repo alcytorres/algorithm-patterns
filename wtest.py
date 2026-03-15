5d29a7839aaa29331563c91df79dffc7ba50ba1d
Alcy Torres <mxalcytorres@gmail.com>
2026-03-15 06:20:40 -0400

    Update solutions

--- wtest.py
+++ wtest.py
@@ -1,2 +1,6 @@
+# Ask Claude, Grok, ChatGPT to name the top 50 most common DSA problems you rec I know.
+# In the prompt find a way to include the ones you alread have pracitced
+# Make a new weighted average list of it all
+
 
 
