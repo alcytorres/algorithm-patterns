e304f862745f49012d8a0013ee57a23ccb74b0d9
Alcy Torres <mxalcytorres@gmail.com>
2026-05-03 23:57:15 -0400

    Update solutions

--- DSA/05_Stacks_and_Queues/05-4_Backspace String Compare.py
+++ DSA/05_Stacks_and_Queues/05-4_Backspace String Compare.py
@@ -1,26 +1,26 @@
 # 844. Backspace String Compare
+"""
+Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
 
-# Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+Note that after backspacing an empty text, the text will continue empty.
 
-# Note that after backspacing an empty text, the text will continue empty.
+Example 1:
+    Input: s = "ab#c", t = "ad#c"
+    Output: true
+    Explanation: Both s and t become "ac".
 
-# Solution: https://leetcode.com/problems/backspace-string-compare/description/
+Example 2:
+    Input: s = "ab##", t = "c#d#"
+    Output: true
+    Explanation: Both s and t become "".
 
-# Example 1:
-    # Input: s = "ab#c", t = "ad#c"
-    # Output: true
-    # Explanation: Both s and t become "ac".
+Example 3:
+    Input: s = "a#c", t = "b"
+    Output: false
+    Explanation: s becomes "c" while t becomes "b".
 
-# Example 2:
-    # Input: s = "ab##", t = "c#d#"
-    # Output: true
-    # Explanation: Both s and t become "".
-
-# Example 3:
-    # Input: s = "a#c", t = "b"
-    # Output: false
-    # Explanation: s becomes "c" while t becomes "b".
-
+Solution: https://leetcode.com/problems/backspace-string-compare/description/
+"""
 
 def backspaceCompare(s, t):
     def build(s):
