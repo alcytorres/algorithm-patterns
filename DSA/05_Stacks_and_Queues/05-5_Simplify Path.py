d303283468ad0ad83babd2e56acd4cba23bac817
Alcy Torres <mxalcytorres@gmail.com>
2026-05-04 23:24:50 -0400

    Update solutions

--- DSA/05_Stacks_and_Queues/05-5_Simplify Path.py
+++ DSA/05_Stacks_and_Queues/05-5_Simplify Path.py
@@ -1,63 +1,64 @@
 # 71. Simplify Path
+"""
+You are given an absolute path for a Unix-style file system, which always begins with a slash '/'. Your task is to transform this absolute path into its simplified canonical path.
 
-# You are given an absolute path for a Unix-style file system, which always begins with a slash '/'. Your task is to transform this absolute path into its simplified canonical path.
+The rules of a Unix-style file system are as follows:
+    A single period '.' represents the current directory.
+    A double period '..' represents the previous/parent directory.
+    Multiple consecutive slashes such as '//' and '///' are treated as a single slash '/'.
+    Any sequence of periods that does not match the rules above should be treated as a valid directory or file name. For example, '...' and '....' are valid directory or file names.
 
-# The rules of a Unix-style file system are as follows:
-    # A single period '.' represents the current directory.
-    # A double period '..' represents the previous/parent directory.
-    # Multiple consecutive slashes such as '//' and '///' are treated as a single slash '/'.
-    # Any sequence of periods that does not match the rules above should be treated as a valid directory or file name. For example, '...' and '....' are valid directory or file names.
+The simplified canonical path should follow these rules:
+    The path must start with a single slash '/'.
+    Directories within the path must be separated by exactly one slash '/'.
+    The path must not end with a slash '/', unless it is the root directory.
+    The path must not have any single or double periods ('.' and '..') used to denote current or parent directories.
+    Return the simplified canonical path.
 
-# The simplified canonical path should follow these rules:
-    # The path must start with a single slash '/'.
-    # Directories within the path must be separated by exactly one slash '/'.
-    # The path must not end with a slash '/', unless it is the root directory.
-    # The path must not have any single or double periods ('.' and '..') used to denote current or parent directories.
-    # Return the simplified canonical path.
+Example 1:
+    Input: path = "/a/b///c/.././d/../f/"
+    Output: "/a/b/f"
+    Explanation:
+    Extra slashes are treated as one.
+    "." means current directory (ignored).
+    ".." moves up one level.
+    Simplified path: "/a/b/f"
 
-# Solution: https://leetcode.com/problems/simplify-path/description/
+Example 2:
+    Input: path = "/home/"
+    Output: "/home"
+    Explanation:
+    The trailing slash should be removed.
 
-# Example 1:
-    # Input: path = "/a/b///c/.././d/../f/"
-    # Output: "/a/b/f"
-    # Explanation:
-    # Extra slashes are treated as one.
-    # "." means current directory (ignored).
-    # ".." moves up one level.
-    # Simplified path: "/a/b/f"
+Example 2:
+    Input: path = "/home//foo/"
+    Output: "/home/foo"
+    Explanation:
+    Multiple consecutive slashes are replaced by a single one.
 
-# Example 2:
-    # Input: path = "/home/"
-    # Output: "/home"
-    # Explanation:
-    # The trailing slash should be removed.
+Example 3:
+    Input: path = "/../"
+    Output: "/"
+    Explanation:
+    Going one level up from the root directory is not possible.
 
-# Example 2:
-    # Input: path = "/home//foo/"
-    # Output: "/home/foo"
-    # Explanation:
-    # Multiple consecutive slashes are replaced by a single one.
+Example 4:
+    Input: path = "/home/user/Documents/../Pictures"
+    Output: "/home/user/Pictures"
+    Explanation:
+    A double period ".." refers to the directory up a level (the parent directory).
 
-# Example 3:
-    # Input: path = "/../"
-    # Output: "/"
-    # Explanation:
-    # Going one level up from the root directory is not possible.
+TLDR:
+Given a Unix-style file path string
+Simplify it to its canonical form by:
+  • Removing redundant slashes (`// → /`)
+  • Ignoring current directory markers (`.`)
+  • Going up one directory for (`..`)
+  • Keeping valid folder names (like `...`)
+Return the final simplified absolute path starting with `/`
 
-# Example 4:
-    # Input: path = "/home/user/Documents/../Pictures"
-    # Output: "/home/user/Pictures"
-    # Explanation:
-    # A double period ".." refers to the directory up a level (the parent directory).
-
-# TLDR:
-# Given a Unix-style file path string
-# Simplify it to its canonical form by:
-#   • Removing redundant slashes (`// → /`)
-#   • Ignoring current directory markers (`.`)
-#   • Going up one directory for (`..`)
-#   • Keeping valid folder names (like `...`)
-# Return the final simplified absolute path starting with `/`
+Solution: https://leetcode.com/problems/simplify-path/description/
+"""
 
 def simplifyPath(path):
     stack = []
