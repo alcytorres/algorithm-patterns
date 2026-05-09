d7663f133dcab7596143c2470f264129b42e8b41
Alcy Torres <mxalcytorres@gmail.com>
2026-05-08 23:27:25 -0400

    Update solutions

--- DSA/04_Linked_Lists/04-3_example_2.py
+++ DSA/04_Linked_Lists/04-3_example_2.py
@@ -1,29 +1,30 @@
 # 141. Linked List Cycle
+"""
+Example 2: Given the head of a linked list, determine if the linked list has a cycle.
 
-# Example 2: Given the head of a linked list, determine if the linked list has a cycle.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
 
-# There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 
-# Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+Return true if there is a cycle in the linked list. Otherwise, return false.
 
-# Return true if there is a cycle in the linked list. Otherwise, return false.
+Example 1
+    Input: head = [1, 2, 3, 4], pos = 1
+    Output: true
+    Explanation: There is a cycle in the linked list, where the tail connects to the node at index 1 (2).
 
-# Solution: https://leetcode.com/problems/linked-list-cycle/description/
+Example 2
+    Input: head = [1, 2], pos = 0
+    Output: true
+    Explanation: There is a cycle in the linked list, where the tail connects to the node at index 0 (1).
 
-# Example 1
-    # Input: head = [1, 2, 3, 4], pos = 1
-    # Output: true
-    # Explanation: There is a cycle in the linked list, where the tail connects to the node at index 1 (2).
+Example 3
+    Input: head = [1], pos = -1
+    Output: false
+    Explanation: There is no cycle in the linked list.
 
-# Example 2
-    # Input: head = [1, 2], pos = 0
-    # Output: true
-    # Explanation: There is a cycle in the linked list, where the tail connects to the node at index 0 (1).
-
-# Example 3
-    # Input: head = [1], pos = -1
-    # Output: false
-    # Explanation: There is no cycle in the linked list.
+Solution: https://leetcode.com/problems/linked-list-cycle/description/
+"""
 
 class ListNode:
     def __init__(self, val):
