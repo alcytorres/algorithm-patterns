0e9140f755486a45a44289db80f6f6a4cbb65916
Alcy Torres <mxalcytorres@gmail.com>
2026-05-10 23:01:21 -0400

    Update solutions

--- DSA/04_Linked_Lists/04-8_Reverse_Linked_List.py
+++ DSA/04_Linked_Lists/04-8_Reverse_Linked_List.py
@@ -1,20 +1,21 @@
 # 206. Reverse Linked List
+"""
+Given the head of a singly linked list, reverse the list, and return the reversed list.
 
-# Given the head of a singly linked list, reverse the list, and return the reversed list.
+Example 1:
+    Input: head = [1, 2, 3]
+    Output: [3, 2, 1]
 
-# Example 1:
-    # Input: head = [1, 2, 3]
-    # Output: [3, 2, 1]
+Example 2:
+    Input: head = [1, 2]
+    Output: [2, 1]
 
-# Example 2:
-    # Input: head = [1, 2]
-    # Output: [2, 1]
+Example 3:
+    Input: head = []
+    Output: []
 
-# Example 3:
-    # Input: head = []
-    # Output: []
-
-# Solution: https://leetcode.com/problems/reverse-linked-list/description/
+Solution: https://leetcode.com/problems/reverse-linked-list/description/
+"""
 
 class ListNode:
     def __init__(self, val=0, next=None):
